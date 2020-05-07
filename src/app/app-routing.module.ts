e5566b81cfb5609c1e4a3cd3fa1f0ec2ec0b83a5
Elmahdi <benohoude@gmail.com>
2020-05-07 05:38:54 +0000

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RendezVousComponent } from 'src/components/rendez-vous/rendez-vous.component';
+import { PatientComponent } from 'src/components/patient/patient.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'patient',component:PatientComponent},
+  {path:'rdv',component:RendezVousComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routineComponents = [PatientComponent,RendezVousComponent]
